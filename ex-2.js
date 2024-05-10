c9c1c35ccf92fd457ab9f6c7a118c600236c6647
Moth <kampanee.moth@gmail.com>
2024-05-10 13:22:01 +0700

    test: add ex-1 to ex-3 .js

--- ex-2.js
+++ ex-2.js
@@ -6,5 +6,14 @@ const product = {
   ratings: 2.5,
   photo: null,
 };
+console.log(product);
 let socialMediaShare = "facebookShare";
 // Start code here
+product["ratings"] = 4.5;
+product["photo"] =
+  "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+console.log(product);
+
+product["code"] = "IT0101010";
+delete product.ratings;
+console.log(product);
